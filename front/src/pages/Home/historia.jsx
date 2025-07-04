63bde739df04fc4bd894725d02760ed78b077f7e
whoaomi59 <whoaomi11@gmail.com>
2025-07-04 13:57:58 -0500

    fundadores

--- front/src/pages/Home/historia.jsx
+++ front/src/pages/Home/historia.jsx
@@ -27,23 +27,17 @@ export default function Historia() {
               </p>
             </div>
 
-            <div class="row">
-              <div class="col-lg-6 col-md-6 col-12">
-                <div class="custom-text-box mb-lg-0">
-                  <h5 class="mb-3">Nuestra misión</h5>
-
-                  <p>
-                    Contribuir al bienestar y desarrollo integral de las
-                    comunidades urbanas y rurales, priorizando a la población
-                    infantil, juvenil, madres cabeza de hogar y adultos mayores,
-                    mediante programas sociales, culturales, educativos, de
-                    salud física y mental, ambientales, recreativos y de
-                    fortalecimiento comunitario. Promovemos la participación
-                    activa, el empoderamiento ciudadano y la construcción de
-                    entornos sanos, seguros y resilientes.
-                  </p>
-                </div>
-              </div>
+            <div class="custom-text-box">
+              <h5 class="mb-3">Nuestra misión</h5>
+              <p class="mb-0">
+                Contribuir al bienestar y desarrollo integral de las comunidades
+                urbanas y rurales, priorizando a la población infantil, juvenil,
+                madres cabeza de hogar y adultos mayores, mediante programas
+                sociales, culturales, educativos, de salud física y mental,
+                ambientales, recreativos y de fortalecimiento comunitario.
+                Promovemos la participación activa, el empoderamiento ciudadano
+                y la construcción de entornos sanos, seguros y resilientes.
+              </p>
             </div>
           </div>
         </div>
